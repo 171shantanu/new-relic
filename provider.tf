f3b4dd9d86db39d07743b528505b357ea32cac43
shantanu <shantanu171@gmail.com>
2023-04-15 17:30:08 +0530

    Added S3 remote backend

--- provider.tf
+++ provider.tf
@@ -6,6 +6,11 @@ terraform {
       version = ">=3.20.2"
     }
   }
+  backend "s3" {
+    bucket = "remotebackend-terraform"
+    key    = "new-relic-backend/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 # Configuring the New Relic provider
